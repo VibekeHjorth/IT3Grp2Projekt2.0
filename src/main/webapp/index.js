e9efc02ad482bebe029f689beab7c2bf0ca0d6e0
Vibeke <VibekeHjorth93@hotmail.com>
2022-12-02 10:48:25 +0100

    Lidt opdateret API

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,17 +1,17 @@
 async function clickLogin() {
 
-    let loginform = document.getElementById("loginform");
+    let loginform = document.getElementById("loginForm");
     const formData = new FormData(loginform);
     const object = Object.fromEntries(formData);
     console.log(object);
 
-    const res = await fetch("api/login", {
+  /*  const res = await fetch("api/login", {
         method: "POST",
         body: JSON.stringify(object),
         headers: {
             "content-type": "application/json"
         }
-    })
+    })*/
 
     window.location.href="desktop.html"
     console.log(res.text());
